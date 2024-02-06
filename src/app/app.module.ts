521ae707cfd34166bac89893b60d229760506188
Ivan S. Boyko <i.boyko@unisoftplus.com>
2024-02-06 23:05:18 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+
+LOAD_WASM().subscribe();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxScannerQrcodeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/news/news.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgxScannerQrcodeComponent } from 'ngx-scanner-qrcode';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent implements AfterViewInit {
+  @ViewChild('camera') protected camera?: NgxScannerQrcodeComponent;
+
+  constructor(private http: HttpClient) { }
+
+  ngAfterViewInit() {
+    const playDeviceFacingBack = (devices: any[]) => {
+      // front camera or back camera check here!
+      const device = devices.find(f => (/back|rear|environment/gi.test(f.label))); // Default Back Facing Camera
+      this.camera?.playDevice(device ? device.deviceId : devices[0].deviceId);
+    }
+    this.camera?.start(playDeviceFacingBack).subscribe((r: any) => console.log(r), alert);
+
+    this.http.get<string>(`https://192.168.1.42:7094/Shifts/GetServerTime`).subscribe();
+  }
+}
